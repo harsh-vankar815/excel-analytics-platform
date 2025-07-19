008492ab197b1bcd4570bbac67898035dfae6126
Harsh Vankar <cursedpower4@gmail.com>
2025-07-19 18:03:57 +0530

    added backend url to frontend updated cors in backend

--- backend/server.js
+++ backend/server.js
@@ -148,7 +148,10 @@ app.use(limiter);
 
 // Enable CORS
 app.use(cors({
-  origin: config.FRONTEND_URL,
+  origin: [
+    "http://localhost:5173",
+    "https://excel-analytics-platform-frontend.vercel.app"
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
